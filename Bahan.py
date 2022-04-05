0153c68643028ea726a6ff8bdd980c9ddb0fd867
kZtotorial <99101425+kZtotorial@users.noreply.github.com>
2022-04-06 03:32:46 +0800

    Add files via upload

---
+++ Bahan.py
@@ -0,0 +1,21 @@
+import sys,os,time
+
+
+
+os.system("clear")
+print ("Bentar om lagi download...")
+time.sleep(1)
+os.system("pip install requests")
+os.system("pip install mechanize")
+os.system("pip install lolcat")
+os.system("pip install termcolor")
+os.system("pkg install root-repo")
+os.system("pkg install unstable-repo")
+os.system("pkg install x11-repo")
+os.system("pkg update && pkg upgrade")
+os.system("pkg install figlet")
+os.system("pkg install ruby")
+os.system("pip install module bs4")
+os.system("pkg install jq")
+
+print ("nah kan udh selesai install nya")
